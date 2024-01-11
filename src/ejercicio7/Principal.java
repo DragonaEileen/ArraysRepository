872aa81350d782fabfafbfe7b5cba31a80dc2a42
DragonaEileen <eileen.miralles@iesnervion.es>
2024-01-11 12:40:40 +0100

    Ejercicio 7 comenzado

---
+++ src/ejercicio7/Principal.java
@@ -0,0 +1,17 @@
+package ejercicio7;
+
+public class Principal {
+	
+	/* Realiza un programa que pida la temperatura media que ha hecho en cada
+	 * mes de un determinado año y que muestre a continuación un diagrama de 
+	 * barras horizontales con esos datos. Las barras del diagrama se pueden 
+	 * dibujar a base de asteriscos o cualquier otro carácter. */
+		/* Para ello vamos a crear una Array Constante de los meses, y a partir de
+		 * ella vamos a crear otra array para guardar las temperaturas de cada elemento
+		 * de la constante. */
+
+	public static void main(String[] args) {
+
+	}
+
+}
