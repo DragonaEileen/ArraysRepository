c1ba17998dc34b3bf8b9ee78954c2e3b8af3b23a
DragonaEileen <eileen.miralles@iesnervion.es>
2024-01-12 14:22:55 +0100

    Empezando ejercicio8

---
+++ src/ejercicio8/Principal.java
@@ -0,0 +1,14 @@
+package ejercicio8;
+
+public class Principal {
+
+	/* Crea un programa que cree un array de enteros de tamaño 100 y lo rellene con 
+	 * valores enteros aleatorios entre 1 y 10 (utiliza 1 + Math.random()*10). Luego 
+	 * pedirá un valor N y mostrará en qué posiciones del array aparece N.  */
+		/**/
+	
+	public static void main(String[] args) {
+
+	}
+
+}
