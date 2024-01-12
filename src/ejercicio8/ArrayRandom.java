20d8bc55d8b5d8f377969d34a3ad72b30af6b611
DragonaEileen <eileen.miralles@iesnervion.es>
2024-01-12 14:39:33 +0100

    Ejercicio 8 por la mitad

---
+++ src/ejercicio8/ArrayRandom.java
@@ -0,0 +1,65 @@
+package ejercicio8;
+
+import java.util.Random;
+
+/**
+ * Esta clase contiene un array la cual podemos rellenar con números aleatorios, y métodos
+ * para ello.
+ * 
+ * @author Eileen
+ */
+public class ArrayRandom {
+
+	/* Declaración de Atributos */
+	/**
+	 * Array de valores random y longitud 100
+	 */
+	int tabla[] = new int[100];
+	
+	/* Métodos */
+	/**
+	 * Este método sirve para rellenar la tabla con numeros aleatorios
+	 */
+	public void fill() {
+		
+		for(int i = 0; i < tabla.length; i++) {
+			
+			tabla[i] = (int) (Math.random()*10);
+			
+		}//Fin FOR --> Relleno
+		
+	}//Fin fill()
+	
+	/**
+	 * Este método recibe un número y lo busca en la tabla. Luego devuelve un array 
+	 * con las posiciones en las que ha encontrado el numero.
+	 * 
+	 * @param num Valor a buscar
+	 * @return posiciones Conjunto de posiciones en las que se encuentra el valor dado
+	 */
+	int[] busqueda(int num) {
+		
+		/* Declaraciones */
+			/* Array en el que guardamos las posiciones en las que se encuentra el numero dado */
+		int posiciones[];
+		
+			/* Declaramos un contador de cuantas veces existe el numero en la tabla */
+		int longitud = 0;
+		
+		/* Para meter números primero necesitamos la longitud de la array a pasar */
+		for(int i = 0; i < tabla.length; i++) {
+			
+			if(tabla[i] == num) {
+				
+				longitud = longitud + 1;
+				
+			}
+			
+		}
+		
+		/* Return */
+		return posiciones;
+		
+	}//Fin busqueda()
+	
+}
--- src/ejercicio8/Principal.java
+++ src/ejercicio8/Principal.java
@@ -5,10 +5,21 @@ public class Principal {
 	/* Crea un programa que cree un array de enteros de tamaño 100 y lo rellene con 
 	 * valores enteros aleatorios entre 1 y 10 (utiliza 1 + Math.random()*10). Luego 
 	 * pedirá un valor N y mostrará en qué posiciones del array aparece N.  */
-		/**/
+		/* Vamos a crear la array en el main, se la pasamos a un metodo para rellenarlo.
+		 * Luego a otro método le pasaremos un número a buscar y nos devolverá un array de
+		 * posiciones. */
 	
 	public static void main(String[] args) {
 
-	}
+		/* Declaraciones */
+			/* Array a rellenar con números aleatorios */
+		int tabla[];
+		
+			/* Array que guarda las posiciones en las que se encuentra un número especifico */
+		int posiciones[];
+		
+		
+		
+	}//FIN MAIN
 
 }
