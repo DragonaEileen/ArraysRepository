1274193ff0ce26ffe3771117d9678109af2150c0
DragonaEileen <eileen.miralles@iesnervion.es>
2024-01-16 14:37:36 +0100

    Start Ej3

---
+++ src/ejercicio3/Principal.java
@@ -0,0 +1,36 @@
+package ejercicio3;
+
+import java.util.Scanner;
+
+public class Principal {
+	
+	/* Crea un programa que pida al usuario 20 valores enteros e introduzca los 10 primeros 
+	 * en un array y los 10 últimos en otro array. Por último, comparará ambos arrays y le 
+	 * dirá al usuario si son iguales o no. */
+
+	public static void main(String[] args) {
+
+		/* Declaraciones */
+			/* Array que contendra los 10 primeros valores */
+		int tabla1[];
+		
+			/* Array que contendrá los 10 últimos valores */
+		int tabla2[];
+		
+		/* Apertura de Scanner */
+		Scanner sc = new Scanner(System.in);
+		
+		/* Construimos Arrays */
+		tabla1 = new int[10];
+		tabla2 = new int[10];
+		
+		/* Algoritmo */
+			/* Recogemos los diez primeros datos */
+		for(int )
+		
+		/* Cierre de Scanner */
+		sc.close();
+		
+	}
+
+}
