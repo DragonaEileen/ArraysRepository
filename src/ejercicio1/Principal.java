94bed73b5b2521c727ca5bfcbc36c3bde6b3a84d
DragonaEileen <eileen.miralles@iesnervion.es>
2024-01-16 13:50:29 +0100

    Start of Arrays con la clase Array

---
+++ src/ejercicio1/Principal.java
@@ -0,0 +1,13 @@
+package ejercicio1;
+
+public class Principal {
+	
+	/* Crea un programa que pida al usuario dos valores N y M y luego cree un array de
+	 * tamaño N que contenga M en todas sus posiciones. Luego muestra el array por pantalla. */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
