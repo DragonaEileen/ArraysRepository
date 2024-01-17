2ddf8f048c851558da5d9f932663adfdb5df927c
DragonaEileen <eileen.miralles@iesnervion.es>
2024-01-17 10:02:21 +0100

    Ejercicio 6 Comenzado

---
+++ src/ejercicio6/Principal.java
@@ -0,0 +1,15 @@
+package ejercicio6;
+
+public class Principal {
+	
+	/* Crea un programa que cree un array de tamaño 1000 y lo rellene con valores enteros 
+	 * aleatorios entre 0 y 99 (utiliza Math.random()*100). Luego pedirá por teclado un 
+	 * valor N y se mostrará por pantalla si N existe en el array, además de cuantas veces.  */
+
+	public static void main(String[] args) {
+
+		/* Declaraciones */
+		
+	}
+
+}
