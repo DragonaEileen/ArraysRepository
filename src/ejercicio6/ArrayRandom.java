ccbf9b8e2a8bc74ca63e72ccd69e3386f622138a
DragonaEileen <eileen.miralles@iesnervion.es>
2024-01-17 10:06:10 +0100

    Added new class on Ejercicio6

---
+++ src/ejercicio6/ArrayRandom.java
@@ -0,0 +1,13 @@
+package ejercicio6;
+
+/**
+ * Clase que guardará un array de tamaño 1000 y numeros aleatorios, 
+ * y métodos para encontrar un número específico.
+ * 
+ * @author Eileen
+ */
+public class ArrayRandom {
+
+	
+	
+}
