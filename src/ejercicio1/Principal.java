43a45dc11e0d3de64853d9a023720e5d8d831dc0
DragonaEileen <eileen.miralles@iesnervion.es>
2024-01-19 13:39:43 +0100

    First Commit Arrays Con Funciones

---
+++ src/ejercicio1/Principal.java
@@ -0,0 +1,13 @@
+package ejercicio1;
+
+public class Principal {
+
+	/* Realiza una función que reciba como parámetro una tabla de enteros y devuelva 
+	 * la suma de todos los valores almacenados en la tabla. Prueba el comportamiento 
+	 * de la función en un método main. */
+	
+	public static void main(String[] args) {
+
+	}
+
+}
