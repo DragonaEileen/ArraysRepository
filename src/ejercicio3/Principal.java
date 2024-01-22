f05fd623b5699c752dd4f80c855a88671c42e8b6
DragonaEileen <eileen.miralles@iesnervion.es>
2024-01-22 08:53:56 +0100

    Ejercicio 3 Completo

--- src/ejercicio3/Principal.java
+++ src/ejercicio3/Principal.java
@@ -7,8 +7,7 @@ public class Principal {
 	/* Escribir la función int[] rellenaPares(int longitud, int fin), que crea y 
 	 * devuelve una tabla ordenada de la longitud especificada, que se encuentra 
 	 * rellena con números pares aleatorios comprendidos entre 2 hasta fin inclusive. */
-		/*  */
-	
+
 	public static void main(String[] args) {
 
 		/* Declaraciones */
