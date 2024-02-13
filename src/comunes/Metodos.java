f4cb0b9d9edd360ec32bc0ffc577bca609a00146
DragonaEileen <eileen.miralles@iesnervion.es>
2024-02-13 14:37:57 +0100

    First Commit

---
+++ src/comunes/Metodos.java
@@ -0,0 +1,144 @@
+package comunes;
+
+/**
+ * Clase que contiene métodos estáticos para usar regularmente 
+ * en los ejercicios de cadenas.
+ * 
+ * @author Eileen
+ */
+public class Metodos {
+
+	/**
+	 * Método para generar un caracter random de la A a la Z
+	 */
+	public static char randomChar() {
+		
+		/* Declaraciones */
+			/* Caracter a devolver por claridad de código */
+		char character;
+		
+			/* Random number to choose character */
+		int randy;
+		
+		/* Randomizer */
+		randy = (int) (Math.random()*(25) + 1);
+		
+		/* Switch to choose character */
+		character = switch (randy) {
+		
+		case 1:{ yield 'a' ;}
+		
+		case 2:{ yield 'b' ;}
+		
+		case 3:{ yield 'c' ;}
+		
+		case 4:{ yield 'd' ;}
+		
+		case 5:{ yield 'e' ;}
+		
+		case 6:{ yield 'f' ;}
+		
+		case 7:{ yield 'g' ;}
+		
+		case 8:{ yield 'h' ;}
+		
+		case 9:{ yield 'i' ;}
+		
+		case 10:{ yield 'j' ;}
+		
+		case 11:{ yield 'k' ;}
+		
+		case 12:{ yield 'l' ;}
+		
+		case 13:{ yield 'm' ;}
+		
+		case 14:{ yield 'n' ;}
+		
+		case 15:{ yield 'o' ;}
+		
+		case 16:{ yield 'p' ;}
+		
+		case 17:{ yield 'q' ;}
+		
+		case 18:{ yield 'r' ;}
+		
+		case 19:{ yield 's' ;}
+		
+		case 20:{ yield 't' ;}
+		
+		case 21:{ yield 'u' ;}
+		
+		case 22:{ yield 'v' ;}
+		
+		case 23:{ yield 'w' ;}
+		
+		case 24:{ yield 'x' ;}
+		
+		case 25:{ yield 'y' ;}
+		
+		case 26:{ yield 'z' ;}
+			
+		default:{yield 'ñ';}
+
+		};
+		
+		/* Return */
+		return character;
+		
+	}//Fin randomChar()
+	
+	/**
+	 * Método que dada una longitud, crea una cadena random de esa longitud
+	 * 
+	 * @param length Longitud de la cadena a generar
+	 * @return cadena Cadena a devolver
+	 */
+	public static String randomString(int length) {
+		
+		/* Declaraciones */
+			/* Cadena a devolver por claridad de código */
+		String cadena = "";
+		
+		/* Operación */
+		//Bucle para añadir carácteres
+		for(int i = 0; i < length; i++) {
+			
+			//Sumamos un character random
+			cadena = cadena + randomChar();
+			
+		}//Fin FOR --> Bucle para añadir caracteres
+		
+		/* Return */
+		return cadena;
+		
+	}//Fin randomString()
+	
+	/**
+	 * Método sobrecargado que no dada una longitud, crea una cadena random de longitud random
+	 * 
+	 * @return cadena Cadena a devolver
+	 */
+	public static String randomString() {
+		
+		/* Declaraciones */
+			/* Cadena a devolver por claridad de código */
+		String cadena = "";
+		
+			/* Valor de la longitud */
+		int randy = (int) (Math.random()*100);
+		
+		/* Operación */
+		//Bucle para añadir carácteres
+		for(int i = 0; i < randy; i++) {
+			
+			//Sumamos un character random
+			cadena = cadena + randomChar();
+			
+		}//Fin FOR --> Bucle para añadir caracteres
+		
+		/* Return */
+		return cadena;
+		
+	}//Fin randomString()
+	
+}
---
+++ src/ejercicio1/Principal.java
@@ -0,0 +1,35 @@
+package ejercicio1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		/* Declaraciones */
+			/* Primera cadena a comparar */
+		String cadena1;
+		
+			/* Segunda cadena a comparar */
+		String cadena2;
+		
+		/* Algoritmo */
+		cadena1 = comunes.Metodos.randomString();
+		cadena2 = comunes.Metodos.randomString();
+		
+		//Comparacion
+		if(cadena1.length() > cadena2.length()) {
+			
+			System.out.println("Cadena 1 es más larga");
+			
+		}else if(cadena2.length() > cadena1.length()){
+			
+			System.out.println("Cadena 2 es más larga");
+			
+		}else{
+			
+			System.out.println("Las cadenas son iguales");
+			
+		}//Fin IF --> Comparator
+		
+	}//FIN MAIN
+
+}
