7299f1ac64550e008fec312bd22a3dee8b7090fa
DragonaEileen <eileen.miralles@iesnervion.es>
2024-02-16 14:27:47 +0100

    Ejercicio 17 comenzado

---
+++ src/ejercicio17/MetodosEj17.java
@@ -0,0 +1,17 @@
+package ejercicio17;
+
+/**
+ * Clase que contiene el método para eliminar comentarios en C
+ * 
+ * @author Eileen
+ */
+public class MetodosEj17 {
+
+	/**
+	 * Método para eliminar comentarios multilinea
+	 * 
+	 * @param cadena Cadena a trimear
+	 */
+	static
+	
+}
---
+++ src/ejercicio17/PrincipalEj17.java
@@ -0,0 +1,16 @@
+package ejercicio17;
+
+public class PrincipalEj17 {
+
+	public static void main(String[] args) {
+
+		/* Declaraciones */
+			/* Cadena de Uso */
+		String cadena = "if (a==3) /* igual a tres */ a++; /* incrementamos a */";
+		
+		/* Operaciones */
+		
+		
+	}
+
+}
