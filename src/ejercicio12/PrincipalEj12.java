7f3ba7d028217a25cd3f3e01ab7bd0d83c96ef56
Cyber Dragons <elemento.gemini@gmail.com>
2024-02-19 17:22:10 +0100

    Ejercicio 12 Completado

---
+++ src/ejercicio12/PrincipalEj12.java
@@ -0,0 +1,31 @@
+package ejercicio12;
+
+public class PrincipalEj12 {
+
+	public static void main(String[] args) {
+
+		/* Declaraciones */
+			/* Cadena sobre la que vamos a trabajar */
+		final String CADENA = "Leer una frase y encontrar la palabra de mayor longitud.";
+		
+			/* Array de cadenas en la que vamos a dividir la CADENA */
+		String[] strArray = CADENA.split(" ");
+		
+			/* Longest substring */
+		String longest = "";
+		
+		//Lets go Across de strArray looking for the longest subString
+		for(String str: strArray) {
+			
+			//If it's larger, we replace it
+			if(str.length() > longest.length()) { longest = str; }
+			
+		}//Fin FOR-EACH --> Longest Search
+		
+		/* Print */
+		System.out.println("The longest word is: " + longest);
+		
+		
+	}
+
+}
