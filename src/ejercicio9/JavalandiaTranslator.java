5f87e8d2ad6b5d3fc986680f753b31e3b27f263d
Cyber Dragons <elemento.gemini@gmail.com>
2024-02-19 20:01:22 +0100

    Ejercicio 9 a medias

---
+++ src/ejercicio9/JavalandiaTranslator.java
@@ -0,0 +1,106 @@
+package ejercicio9;
+
+
+/**
+ * Clase que contiene las funciones necesarias para traducir del javalandes
+ */
+public class JavalandiaTranslator {
+
+	private static final String HYBRID = "HYBRID";
+
+	private static final String SPECIAL = "SPECIAL";
+
+	private static final String STANDARD = "STANDARD";
+
+	/* Atributos */
+	/**
+	 * Muletilla del dialecto estándar
+	 */
+	final String STANDARD_DIALECT = "javalín, javalón";
+	
+	/**
+	 * Muletilla del dialecto especial
+	 */
+	final String UNIQUE_DIALECT = "javalén, len, len";
+
+	/**
+	 * Método para comprobar a que dialecto pertenece una cadena dada
+	 * 
+	 * @param CADENA Cadena a comprobar a que dialecto pertenece
+	 * @return dialect String that says which dialect is being used
+	 */
+	String dialectObserver(final String CADENA) {
+		
+		/* Declarations */
+			/* PCC: String tht we are gonna return */
+		String dialect = "";
+		
+		/* Operaciones */
+		//Let's see if it's a "javalín javalón" or a "javalén"
+		if(CADENA.contains(STANDARD_DIALECT)) {
+			
+			dialect = STANDARD;
+			
+		}else if(CADENA.contains(UNIQUE_DIALECT)){
+			
+			dialect = SPECIAL;
+			
+		}else if(CADENA.contains(STANDARD_DIALECT) && CADENA.contains(UNIQUE_DIALECT)) {
+			
+			dialect = HYBRID;
+			
+		}//Fin IF --> Dialect Observing
+		
+		/* Return */
+		return dialect;
+		
+	}//Fin dialectObserver()
+	
+	/**
+	 * Método para translate a phrase a español
+	 * 
+	 * @param CADENA Cadena a translate
+	 * @return strTranslated Cadena traducida
+	 */
+	String translate(final String CADENA) {
+		
+		/* Declaraciones */
+			/* PCC: Cadena a return */
+		String strTranslated = "";
+		
+			/* Cadena que indica el dialecto */
+		String dialect = dialectObserver(CADENA);
+		
+		/* Operaciones */
+		switch (dialect) {
+		case STANDARD: 
+			
+			//Reemplazamos la muletilla del principio
+			strTranslated = CADENA.replace(STANDARD_DIALECT, "");
+			
+		case SPECIAL:
+			
+			//Reemplazamos la muletilla del final
+			strTranslated = CADENA.replace(UNIQUE_DIALECT, ".");
+			
+		case HYBRID:
+			
+			//Reemplazamos la muletilla del principio
+			strTranslated = CADENA.replace(STANDARD_DIALECT, "");
+			
+			//Luego la del final
+			strTranslated = strTranslated.replace(UNIQUE_DIALECT, ".");
+			
+		default:
+			
+			//No reemplazamos nada
+			strTranslated = CADENA;
+			
+		}//Fin SWITCH --> Dialect Trimming
+		
+		/* Return */
+		return strTranslated;
+		
+	}//Fin translate()
+	
+}
--- src/ejercicio9/PrincipalEj9.java
+++ src/ejercicio9/PrincipalEj9.java
@@ -3,7 +3,28 @@ package ejercicio9;
 public class PrincipalEj9 {
 
 	public static void main(String[] args) {
-
+		
+		/* Declaraciones */
+			/* Cadena de Dialecto General de Javalandia */
+		final String STANDARD_EXAMPLE = "Javalín, javalón que buen día hace hoy.";
+				
+			/* Cadena de Dialecto Especial de Javalandia */
+		final String UNIQUE_EXAMPLE = "Que buen día hace hoy javalén, len, len"	;
+		
+			/* Cadena de Dialecto Hýbrido */
+		final String HYBRID_EXAMPLE = "Javalín, javalón Que buen día hace hoy javalén, len, len";
+		
+			/* Objeto traductor */
+		JavalandiaTranslator translator = new JavalandiaTranslator();
+		
+			/* Cadena de prueba */
+		String test = "";
+		
+		/* Operaciones */
+		test = translator.translate(STANDARD_EXAMPLE.toLowerCase());
+		System.out.println(test);
+		
+		
 	}
 
 }
