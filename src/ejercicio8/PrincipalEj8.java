c6a675091dbf6edb35f660b015757046021b803f
DragonaEileen <eileen.miralles@iesnervion.es>
2024-02-20 13:24:34 +0100

    Ejercicio 8 Funciona

--- src/ejercicio8/PrincipalEj8.java
+++ src/ejercicio8/PrincipalEj8.java
@@ -1,34 +1,73 @@
 package ejercicio8;
 
 import comunes.Common;
+import java.util.Arrays;
 
 public class PrincipalEj8 {
 
 	public static void main(String[] args) {
 
 		/* Declaraciones */
-		//First String
-		Common.str1 = "Darkness cannot drive out darkness: only light"
+			//First String
+		String str1 = "Darkness cannot drive out darkness: only light"
 				+ " can do that. Hate cannot drive out hate: only "
 				+ "love can do that.";
 		
 		//Second String
-		Common.str2 = "Dabale arroz a la zorra el abad";
+		String str2 = "Dabale arroz a la zorra el abad";
+		
+		//Third String
+		String str3 = "";
+		
+			/* Cadena Auxiliar */
+		String strAux;
+		
+			/* Str Array Auxiliar */
+		String[] strArray;
 		
 		/* Operación */
-		//Primera cadena
+		//Primera cadena, prim
+		//Generalizamos la cadena
+		strAux = str1.trim().toLowerCase();
+		
+		//Eliminamos los espacios
+		strArray = strAux.split(" ");
+		
+		//Volvemos a generalizar
+		strAux = "";
+		for(String str: strArray) {
+			
+			strAux = strAux + str;
+
+		}//Fin FOR EACH cadena
+
+
 		//Eliminamos los espacios con trim() y la invertimos
-		Common.str3 = Common.invert(Common.str1.trim());
+		str3 = Common.invert(strAux);
 		
 		//Comprobación
-		if(Common.str3.equals(Common.str1)) {System.out.println("Success 1");
+		if(str3.equals(strAux)) {System.out.println("Success 1");
 		
 		}else {System.out.println("Not a Success 1");}
 		
 		//Segunda Cadena
-		Common.str3 = Common.invert(Common.str2.trim());
+		//Generalizamos la cadena
+		strAux = str2.trim().toLowerCase();
 		
-		if(Common.str3.toLowerCase().equals(Common.str2.toLowerCase())) {System.out.println("Success 2");
+		//Eliminamos los espacios
+		strArray = strAux.split(" ");
+		
+		//Volvemos a generalizar
+		strAux = "";
+		for(String str: strArray) {
+			
+			strAux = strAux + str;
+
+		}//Fin FOR EACH cadena
+		
+		str3 = Common.invert(strAux);
+		
+		if(str3.equals(strAux)) {System.out.println("Success 2");
 		
 		}else {System.out.println("Not a Success 2");}
 		
